54570d7ba4a7eac3c8a19b0d06b094655cca326f
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-22 02:10:36 +0300

    Deleted the endpoint again!

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello Eyego');
 });
 
-// endpoint to say hello to a user given in the query string
-app.get('/hello', (req, res) => {
-    const name = req.query.name || 'World';
-    res.send(`Hello ${name}`);
-});
-
 try {
     app.listen(3000, () => {
         console.log('Server is running on http://localhost:3000');
