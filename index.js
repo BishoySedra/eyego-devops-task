3bf2373e9c22ad450430312e1056529ba2553804
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-22 20:18:01 +0300

    Deleted the new endpoint!

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello Eyego');
 });
 
-app.get("/hello", (req, res) => {
-    res.send(`Hello, ${req.query.name}`);
-});
-
 try {
     app.listen(3000, () => {
         console.log('Server is running on http://localhost:3000');
