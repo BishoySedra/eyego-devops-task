2fd239f74f2fe9227b4de5b5ddfacccff78b3588
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-22 03:59:25 +0300

    Removed changes from source code a little bit!

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello Eyego');
 });
 
-// endpoint to say hello to a user
-app.get('/hello/:name', (req, res) => {
-    const name = req.params.name;
-    res.send(`Hello ${name}`);
-});
-
 try {
     app.listen(3000, () => {
         console.log('Server is running on http://localhost:3000');
